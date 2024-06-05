2d49745f2ee3001b684fe0b88e2a6d60f3227b9b
Ricardo Guerra <RikkuG491@gmail.com>
2024-06-05 14:22:38 -0400

    Renamed assets folder as images folder, removed unused code from index.html, and updated burgers array in index.js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const burgers = [
     {
         name: "Flatburger",
-        image: "./assets/burgers/flatburger.jpeg",
+        image: "images/burgers/flatburger.jpeg",
         description: "Our signature Flatburger which is a super delicious cheeseburger that'll leave you wanting another Flatburger! It's too good to be true!"
     },
     {
         name: "Maple Bacon Burger",
-        image: "./assets/burgers/maple-bacon-burger.jpeg",
+        image: "images/burgers/maple-bacon-burger.jpeg",
         description: "A great option for a breakfast burger or a great option for brunch if you're hungry enough to eat 2 of these burgers!"
     },
     {
         name: "Mushroom Burger",
-        image: "./assets/burgers/mushroom-burger.webp",
+        image: "images/burgers/mushroom-burger.webp",
         description: "A mushroom burger with our exclusive Flatburger melted cheese!"
     },
     {
         name: "Avocado Bun Burger",
-        image: "./assets/burgers/avocado-bun-burger.jpeg",
+        image: "images/burgers/avocado-bun-burger.jpeg",
         description: "A healthier take on our signature Flatburger but with avocado buns!"
     },
     {
         name: "Ramen Burger",
-        image: "./assets/burgers/ramen-burger.jpeg",
+        image: "images/burgers/ramen-burger.jpeg",
         description: "If you love ramen and burgers, what are you waiting for? Order our exclusive ramen burger now!"
     }
 ]
